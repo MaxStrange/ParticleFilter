02d252b6579ecb353bc61e53a2c137caeed319aa
Max Strange <maxfieldstrange@gmail.com>
2019-05-29 21:34:53 -0700

    Remove binary and update a bit

--- unoptimized/pf.cpp
+++ unoptimized/pf.cpp
@@ -10,7 +10,7 @@ const int SCREEN_WIDTH  = 640;
 const int SCREEN_HEIGHT = 480;
 
 /** Exit macro: If not predicate, print errormsg and exit. */
-#define CHECK_ERROR(predicate, errormsg) do \
+#define CHECK(predicate, errormsg) do \
     {\
         if (!(predicate))\
         {\
@@ -22,12 +22,14 @@ const int SCREEN_HEIGHT = 480;
 
 int main(void)
 {
-    CHECK_ERROR(SDL_Init(SDL_INIT_VIDEO) >= 0, "SDL could not initialize!");
+    auto err = SDL_Init(SDL_INIT_VIDEO);
+    CHECK(err >= 0, "SDL could not initialize!");
 
     SDL_Window *window = SDL_CreateWindow("Particle Filter (Unoptimized)", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
-    CHECK_ERROR(window != NULL, "Window could not be created!");
+    CHECK(window != nullptr, "Window could not be created!");
 
     SDL_Surface *screensurface = SDL_GetWindowSurface(window);
+    CHECK(screensurface != nullptr, "Could not get screen surface.");
 
     // Fill with white
     SDL_FillRect(screensurface, nullptr, SDL_MapRGB(screensurface->format, 0xFF, 0xFF, 0xFF));
